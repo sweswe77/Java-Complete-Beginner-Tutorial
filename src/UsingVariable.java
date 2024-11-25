b57f59d7ee9735555501c7142482e598ad743251
Yuki <swesweaung.ucstgi@gmail.com>
2024-11-25 23:24:49 +0630

    Added a java file

---
+++ src/UsingVariable.java
@@ -0,0 +1,11 @@
+
+public class UsingVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String var="Hello World!";
+		System.out.println(var);
+
+	}
+
+}
